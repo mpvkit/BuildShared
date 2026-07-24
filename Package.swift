5e5589efbeb15babb3593ba6a5327b014a1f24f4
cxfksword <718792+cxfksword@users.noreply.github.com>
2026-07-24 12:32:39 +0800

    chore: update min version not update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BuildShared",
     platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v15)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
